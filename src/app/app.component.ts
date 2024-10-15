71591534eaf95e497642347b5b264db957d56414
CarlosCatari <carlos864541@gmail.com>
2024-10-15 12:39:43 -0500

    add main and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { MainComponent } from './componentes/main/main.component';
+import { ContactComponent } from './componentes/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <app-navbar></app-navbar>
+    <app-main></app-main>
+    <app-contact></app-contact>
+  `,
+  imports: [NavbarComponent, MainComponent, ContactComponent],
 })
 export class AppComponent {
   title = 'portafolio';
