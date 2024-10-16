596a09a63645d9950d5ef12d881e109b338b4031
CarlosCatari <carlos864541@gmail.com>
2024-10-15 21:32:15 -0500

    add components portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { MainComponent } from './componentes/main/main.component';
 import { ContactComponent } from './componentes/contact/contact.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
+import { AboutmeComponent } from './componentes/aboutme/aboutme.component';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +13,20 @@ import { ContactComponent } from './componentes/contact/contact.component';
   template: `
     <app-navbar></app-navbar>
     <app-main></app-main>
+    <app-aboutme></app-aboutme>
+    <app-projects></app-projects>
     <app-contact></app-contact>
+    <app-footer></app-footer>
+
   `,
-  imports: [NavbarComponent, MainComponent, ContactComponent],
+  imports: [
+    NavbarComponent,
+    MainComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
 })
 export class AppComponent {
   title = 'portafolio';
