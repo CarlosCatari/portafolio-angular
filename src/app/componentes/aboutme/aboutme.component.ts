f3b4bbdfa19b5b9506bc7f179125c4195190de38
CarlosCatari <carlos864541@gmail.com>
2024-10-16 19:16:20 -0500

    add images profile

--- src/app/componentes/aboutme/aboutme.component.ts
+++ src/app/componentes/aboutme/aboutme.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aboutme',
   standalone: true,
-  imports: [],
   templateUrl: './aboutme.component.html',
-  styleUrl: './aboutme.component.css'
+  styleUrls: ['./aboutme.component.css']
 })
 export class AboutmeComponent {
-
+  
 }
